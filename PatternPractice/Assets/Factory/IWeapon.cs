e261765e5f88db0d272509eea6e7db454a1dfc8f
Kevin Chang <kevin@megalive.com.tw>
2018-12-10 17:48:52 +0800

    [Add] Weapon 抽象與實現

---
+++ PatternPractice/Assets/Factory/IWeapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimpleFactory
+{
+	public interface Weapon
+	{
+		void Prepare();
+		void Shot();
+	}
+
+	public class HandGun : Weapon
+	{
+		public virtual void Prepare()
+		{
+		}
+
+		public virtual void Shot()
+		{
+		}
+	}
+	
+}
