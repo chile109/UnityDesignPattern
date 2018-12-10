506d586f1af33de297185e4872e1c4ef194f4ff2
Kevin Chang <kevin@megalive.com.tw>
2018-12-10 17:56:52 +0800

    [Add] (commit上下調換)Store 類別為client, 暴露buy方法根據使用者輸入參數回傳不同weapon

---
+++ PatternPractice/Assets/Factory/WeaponFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SimpleFactory
+{
+	public class GunFactory
+	{
+		public Weapon CreateWeapon(string style) {
+			Weapon weapon = null;
+			switch (style)
+			{
+				case "Gun":
+					weapon = new HandGun();
+					break;
+			}
+			return weapon;
+		}
+	}
+}
