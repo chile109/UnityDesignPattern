e2181ae06741f46bd14c98d1cd62559c2bb1b6f4
Kevin Chang <kevin@megalive.com.tw>
2018-12-10 17:51:06 +0800

    [Add] Factory 類別, 封裝 weapon 產出行為

---
+++ PatternPractice/Assets/Factory/GunStore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimpleFactory
+{
+	public class GunStroe : MonoBehaviour
+	{
+		private readonly GunFactory _factory = new GunFactory();
+
+		public GunStroe(GunFactory factory)
+		{
+			this._factory = factory;
+		}
+
+		public Weapon BuyWeapon(String type)
+		{
+			Weapon weapon = _factory.CreateWeapon("Gun");
+			weapon.Prepare();
+			weapon.Shot();
+
+			return weapon;
+		}
+
+		
+	}
+
+}
+
