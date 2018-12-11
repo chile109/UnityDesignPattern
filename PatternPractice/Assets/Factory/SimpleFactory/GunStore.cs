0d144663e919636bec9f5f36fc1db103f11edaf9
Kevin Chang <kevin@megalive.com.tw>
2018-12-11 17:21:05 +0800

    [Move] 調整simple factory資源位置與namespace

--- PatternPractice/Assets/Factory/SimpleFactory/GunStore.cs
+++ PatternPractice/Assets/Factory/SimpleFactory/GunStore.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-namespace SimpleFactory
+namespace Factory.SimpleFactory
 {
 	public class GunStore
 	{
--- PatternPractice/Assets/Factory/SimpleFactory/WeaponFactory.cs
+++ PatternPractice/Assets/Factory/SimpleFactory/WeaponFactory.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-namespace SimpleFactory
+namespace Factory.SimpleFactory
 {
 	public class GunFactory: MonoBehaviour
 	{
