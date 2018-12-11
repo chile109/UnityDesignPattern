9eb54703a64921aa2347963c8955637dc0428e4d
Kevin Chang <kevin@megalive.com.tw>
2018-12-11 18:29:12 +0800

    [Add] FactoryMethod Pattern WeaponPart 抽象與實作類別

---
+++ PatternPractice/Assets/Factory/FactoryMethod/IWeaponPart.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Factory.FactoryMethod
+{
+	public interface WeaponPart
+	{
+		void Prepare();	//Action of providing weapon
+	}
+
+	public class Gun : MonoBehaviour, WeaponPart
+	{
+		public void Prepare()
+		{
+			Debug.Log("Making Gun!");
+		}
+
+	}
+	
+	public class Bullet : MonoBehaviour, WeaponPart
+	{
+		public void Prepare()
+		{
+			Debug.Log("Making Bullet!");
+		}
+	}
+	
+	public class Bow : MonoBehaviour, WeaponPart
+	{
+		public void Prepare()
+		{
+			Debug.Log("Making Bow!");
+		}
+
+	}
+	
+	public class Arrow : MonoBehaviour, WeaponPart
+	{
+		public void Prepare()
+		{
+			Debug.Log("Making Arrow!");
+		}
+	}
+
+}
