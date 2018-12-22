5560e0d6962f61aa67dec3fb7290c27c73579615
Kevin Chang <kevin@megalive.com.tw>
2018-12-22 18:28:41 +0800

    [Add] 產品工廠

---
+++ PatternPractice/Assets/Factory/AbstractFactory/WeaponFactory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Factory.FactoryMethod;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Factory.AbstractFactory
+{
+	public interface WeaponFactory
+	{
+		Launcher CreateLauncher();
+		Projectile CreateProjectile();
+	}
+
+	public class FirearmFactory : WeaponFactory
+	{
+		public Launcher CreateLauncher()
+		{
+			return new ShotGun();
+		}
+
+		public Projectile CreateProjectile()
+		{
+			return new Ammo();
+		}
+	}
+	
+	public class ColdSteelFactory : WeaponFactory
+	{
+		public Launcher CreateLauncher()
+		{
+			return new XBow();
+		}
+
+		public Projectile CreateProjectile()
+		{
+			return new Arrow();
+		}
+	}
+}
