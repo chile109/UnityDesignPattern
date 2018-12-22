e8323c6ed5cc81a5addb645f824b3af63b9ab2bc
Kevin Chang <kevin@megalive.com.tw>
2018-12-22 18:25:50 +0800

    [Add] 產品族類

---
+++ PatternPractice/Assets/Factory/AbstractFactory/WepaonSet.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security;
+using UnityEngine;
+
+namespace Factory.AbstractFactory
+{
+	public abstract class Launcher: MonoBehaviour
+	{
+		public String magazine = "";
+
+		public abstract void Reload(Projectile projectile);
+	}
+
+	public abstract class Projectile
+	{
+		public string name = "";
+	}
+
+	public class ShotGun : Launcher
+	{
+		public override void Reload(Projectile projectile)
+		{
+			var instance = new GameObject().AddComponent<ShotGun>();
+			instance.name = this.GetType().Name;
+			instance.magazine = projectile.name;
+		}
+	}
+	
+	public class XBow : Launcher
+	{
+		public override void Reload(Projectile projectile)
+		{
+			var instance = new GameObject().AddComponent<Launcher>();
+			instance.name = this.GetType().Name;
+			instance.magazine = projectile.name;
+		}
+	}
+
+	public class Ammo : Projectile
+	{
+		public Ammo()
+		{
+			name = "Ammo";
+		}
+	}
+	
+	public class Arrow : Projectile
+	{
+		public Arrow()
+		{
+			name = "Arrow";
+		}
+	}
+}
