61698023b5a1612c97b4a3ff766df3e8361998aa
Kevin Chang <kevin@megalive.com.tw>
2018-12-22 16:07:58 +0800

    [Add] client & store builder

---
+++ PatternPractice/Assets/Factory/AbstractFactory/AbstractFactoryExample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Factory.AbstractFactory
+{
+	public class AbstractFactoryExample : MonoBehaviour
+	{
+		private WeaponStore store;
+		// Use this for initialization
+		void Start()
+		{
+			store = new FirearmStore();
+			//store = new ColdSteelStore();
+			store.OrderWeapon();
+		}
+
+	}
+}
---
+++ PatternPractice/Assets/Factory/AbstractFactory/Store.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Factory.AbstractFactory
+{
+
+	//lets a class defer instantiation to child classes. 
+	public abstract class WeaponStore: MonoBehaviour
+	{
+		public Launcher Gun;
+		public Projectile Bullet;
+		public WeaponFactory factory;
+		
+		public void OrderWeapon()
+		{
+			Gun.Reload(Bullet);
+		}
+	}
+
+	public class FirearmStore: WeaponStore
+	{
+		public FirearmStore()
+		{
+			factory = new FirearmFactory();
+			Gun = factory.CreateLauncher();
+			Bullet = factory.CreateProjectile();
+		}
+	}
+	
+	public class ColdSteelStore: WeaponStore
+	{
+		public ColdSteelStore()
+		{
+			factory = new ColdSteelFactory();
+			Gun = factory.CreateLauncher();
+			Bullet = factory.CreateProjectile();
+		}
+	}
+}
+
